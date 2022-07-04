9c2425cfe6f0e8d20d6d6a81d7714292aa83adcc
Gregory Presser <gpress2222@gmail.com>
2022-07-04 18:56:06 -0400

    First running

---
+++ prisma/migrations/20220704225114_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_UserFollows` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_UserFollows_AB_unique`(`A`, `B`),
+    INDEX `_UserFollows_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
